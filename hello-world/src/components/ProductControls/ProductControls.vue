28159430f0a11cf3f24aebc070f4ff24217d0959
yulleeki <yulleeki@usc.edu>
2019-02-12 10:43:45 +0900

    first commit

---
+++ hello-world/src/components/ProductControls/ProductControls.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="products">
+      <ul class="item">
+        <ProductControl v-for="(product, i) in products" :key="i"
+        :product="product"/>
+      </ul>
+    </section>
+</template>
+
+<script>
+import ProductControl from './ProductControl';
+
+export default {
+    props: {
+        products: {
+            type: Array,
+            default: []
+        }
+    },
+    components: {
+        ProductControl
+    },
+    data() {
+        return {
+            items: 5
+        };
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .item {
+        display: flex;
+        justify-content: start;
+        flex-wrap: wrap;
+    }
+</style>
