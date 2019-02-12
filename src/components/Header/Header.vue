fa417d2e80743a286b280d9bb065ed0fe23dcb6e
yulleeki <yulleeki@usc.edu>
2019-02-12 13:31:51 +0900

    second commit

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+      <img alt="Vue logo" src="../../assets/logo.png" class="logo">
+      <ul class="navigation-items">
+        <li>상품목록</li>
+        <li>장바구니</li>
+        <li>주문목록</li>
+      </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+  .logo {
+    height: 40px;
+  }
+
+  .header {
+    // width height값 설정 가능 inline block 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;//flex-start 
+    align-items: center;
+    //row; // default 값 
+    .navigation-items {
+      display: flex;
+        li {
+            list-style: none;
+            width: 80px;
+            background-color: #ccc;
+            color: #fff;
+            height: 40px;
+            padding: 10px;
+            margin-right: 10px;
+            text-align: center;
+            line-height: 40px;
+        }
+    }
+  }
+
+</style>
---
+++ src/components/ProductControls/Options.vue
@@ -0,0 +1,15 @@
+<template>
+    <select>
+        <option v-for="(option, i) in product.options" :key="i">
+            {{ option.color }} {{ option.size }} 재고: {{ option.stock }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: {
+        product: Object
+    }
+}
+</script>
