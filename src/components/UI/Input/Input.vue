678cc6686c4483677625e59da93ea3057f63fe3c
uoon-dev <uoon.dev@gmail.com>
2019-02-25 21:00:39 +0900

    merge

---
+++ src/components/UI/Input/Input.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-if="type === 'input'" type="text">
+    <select v-else>
+      <option
+        v-for="(option, i) in options"
+        :key="i"
+      >{{ option.color }} {{ option.size }} 재고: {{ option.stock }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      default: []
+    }
+  }
+};
+</script>
